5eaa9498509250b84e9b77ea3fa337592718ea4f
CrazyImSoFlutter <65299607+CrazyImSoFlutter@users.noreply.github.com>
2020-08-12 11:04:08 +0900

    Baekjun C++ 2438

---
+++ C++/BaekjunC++2438.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int N = 0;
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+    {
+        for (int i2 = 0 ; i2 < i; i2++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
