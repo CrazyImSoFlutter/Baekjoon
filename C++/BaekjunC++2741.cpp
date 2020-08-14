94e95273e0bbb49f05a1e2c0cf5afefabeca1319
CrazyImSoFlutter <65299607+CrazyImSoFlutter@users.noreply.github.com>
2020-08-14 15:17:57 +0900

    Baekjun C++ 2741

---
+++ C++/BaekjunC++2741.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<ios>
+using namespace std;
+
+int main ()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    int N = 0;
+    cin >> N;
+    
+    for (int i = 1; i<=N; i++)
+    {
+        cout << i << "\n";
+    }
+}
