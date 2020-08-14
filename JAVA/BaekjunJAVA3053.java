fd2083134935c13865a50b50aee4182648565d9c
CrazyImSoFlutter <65299607+CrazyImSoFlutter@users.noreply.github.com>
2020-08-14 15:28:37 +0900

    Baekjun JAVA 3053

---
+++ JAVA/BaekjunJAVA3053.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+	private static final Scanner sc = new Scanner(System.in);
+	private static final int R = sc.nextInt();
+	public static void main(String args[])
+	{
+		System.out.println(Math.PI*R*R);
+		System.out.println(2*R*R);
+	}
+}
