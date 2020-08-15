cbfee9e1de79cfe61fb323230f5116c658f5ef3b
CrazyImSoFlutter <65299607+CrazyImSoFlutter@users.noreply.github.com>
2020-08-16 00:33:40 +0900

    Baekjun C++ 9498

---
+++ C++/BaekjunC++9498.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	int a = 0;
+	cin >> a;
+    if(a>=90 && a<=100)
+        cout<<"A";
+    else if(a>=80 && a<=89)
+        cout<<"B";
+    else if(a>=70 && a<=79)
+        cout<<"C";
+    else if(a>=60 && a<=69)
+        cout<<"D";
+    else if(a<=59)
+        cout<<"F";
+}
