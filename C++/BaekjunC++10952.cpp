c8e6e4cffc42df7c44fb3285459aab978a8031c1
CrazyImSoFlutter <65299607+CrazyImSoFlutter@users.noreply.github.com>
2020-08-16 15:51:48 +0900

    Baekjun C++ 10952

---
+++ C++/BaekjunC++10952.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int A = 0; 
+    int B = 0;
+    while(1)
+    {
+        cin >> A >> B;
+        if(A == 0 && B == 0)
+        {
+            break;
+        }
+        else 
+            cout << A+B <<endl;
+        
+    }
+}
