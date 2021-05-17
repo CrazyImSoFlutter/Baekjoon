9eb554aca42355e876b66ccf054565792ff2c056
CrazyImSoFlutter <nhg1113@naver.com>
2021-05-18 02:16:29 +0900

    swift

---
+++ C/BaekjunC18870.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int arr[1000001];
+int arr[]
+
+int compare(int a, int b){
+	if (a < b)
+		return -1;
+	else if (a > b)
+		return 1;
+	else
+		return 0;
+}
+
+int main(){
+	
+}
