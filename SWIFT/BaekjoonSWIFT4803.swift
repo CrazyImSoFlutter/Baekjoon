6e450aed54564d09bfc0e97b80259c622b3205cd
CrazyImSoFlutter <nhg1113@naver.com>
2021-09-08 17:38:31 +0900

    re

--- SWIFT/BaekjoonSWIFT4803.swift
+++ SWIFT/BaekjoonSWIFT4803.swift
@@ -1,66 +1,55 @@
 var nm = [Int]()
-var caseCnt = 0
-
-func dfs(_ tree: [[Int]], _ visit: inout [Bool], _ node: Int, _ cnt: inout Int, _ vtx: inout Int) {
-    var flag = false
-    visit[node] = true
-    vtx += 1
-    if tree[node].count == 0 {
-        return
-    }
-    for i in 0..<tree[node].count {
-        if tree[node].count > 1 {
-            for j in tree[node] {
-                if visit[j] == false {
-                    flag = true
-                    break
-                }
-            }
-            if flag == false {
-                cnt += 1
-            }
-        }
-        if visit[tree[node][i]] == false {
-            cnt += 1
-            dfs(tree, &visit, tree[node][i], &cnt, &vtx)
-        }
-    }
-}
+var caseCount = 0
 
 while true {
     nm = readLine()!.split(separator: " ").map { Int(String($0))! }
+   
     let n = nm[0]
     let m = nm[1]
-    var tree = Array(repeating: [Int](), count: n + 1)
-    var visit = Array(repeating: false, count: n + 1)
-    var count = 0
-    var vertex = 0
-    var treeCount = 0
-    caseCnt += 1
-    if nm[0] == 0 && nm[1] == 0 {
+    if n == 0, m == 0 {
         break
     }
+    var tree = Array(repeating: [Int](), count: n + 1)
+    var visit = Array(repeating: false, count: n + 1)
+    var treeCount = 0
+    caseCount += 1
+
     for i in 0..<m {
         let tmp = readLine()!.split(separator: " ").map { Int(String($0))! }
         tree[tmp[0]].append(tmp[1])
         tree[tmp[1]].append(tmp[0])
     }
+
     for i in 1...n {
-        if visit[i] == true {
+        if !visit[i] {
+            if dfs(tree, &visit, i, 0) {
+                treeCount += 1
+            }
+        }
+    }
+
+    if treeCount > 1 {
+        print("Case \(caseCount): A forest of \(treeCount) trees.")
+    } else if treeCount == 1 {
+        print("Case \(caseCount): There is one tree.")
+    } else if treeCount == 0 {
+        print("Case \(caseCount): No trees.")
+    }
+}
+
+func dfs(_ tree: [[Int]], _ visit: inout [Bool], _ node: Int, _ postNode: Int) -> Bool {
+    visit[node] = true
+    
+    for i in 0..<tree[node].count {
+        if tree[node][i] == postNode {
             continue
         }
-        count = 0
-        vertex = 0
-        dfs(tree, &visit, i, &count, &vertex)
-        if count == vertex - 1 {
-            treeCount += 1
+        if visit[tree[node][i]] {
+            return false
+        }
+        if dfs(tree, &visit, tree[node][i], node) == false {
+            return false
         }
     }
-    if treeCount == 0 {
-        print("Case \(caseCnt): No trees.")
-    } else if treeCount == 1 {
-        print("Case \(caseCnt): There is one tree.")
-    } else {
-        print("Case \(caseCnt): A forest of \(treeCount) trees.")
-    }
+    return true
 }
