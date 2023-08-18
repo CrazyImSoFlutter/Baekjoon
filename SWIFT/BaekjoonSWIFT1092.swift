d44a4399721c9ddaa3028cad3f34eed39c12f771
NOGUEN <nhg1113@naver.com>
2023-08-18 14:57:09 +0900

    ^^

--- SWIFT/BaekjoonSWIFT1092.swift
+++ SWIFT/BaekjoonSWIFT1092.swift
@@ -1,86 +1,26 @@
-import Foundation
+func solve() -> Int {
+    var N = Int(readLine()!)!
+    var crane = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted(by: >)
+    var M = Int(readLine()!)!
+    var box = readLine()!.split(separator: " ").map { Int(String($0))! }.sorted(by: >)
+    var answer = 0
 
-// 라이노님의 FileIO
-final class FileIO {
-    private var buffer:[UInt8]
-    private var index: Int
-
-    init(fileHandle: FileHandle = FileHandle.standardInput) {
-        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)]
-        index = 0
+    if box[0] > crane[0] {
+        return -1
     }
 
-    @inline(__always) private func read() -> UInt8 {
-        defer { index += 1 }
-
-        return buffer.withUnsafeBufferPointer { $0[index] }
-    }
-
-    @inline(__always) func readInt() -> Int {
-        var sum = 0
-        var now = read()
-        var isPositive = true
-
-        while now == 10
-                || now == 32 { now = read() }
-        if now == 45{ isPositive.toggle(); now = read() }
-        while now >= 48, now <= 57 {
-            sum = sum * 10 + Int(now-48)
-            now = read()
+    while !box.isEmpty {
+        answer += 1
+        for i in 0..<crane.count {
+            for j in 0..<box.count {
+                if crane[i] >= box[j] {
+                    box.remove(at: j)
+                    break
+                }
+            }
         }
-
-        return sum * (isPositive ? 1:-1)
     }
-
-    @inline(__always) func readString() -> String {
-        var str = ""
-        var now = read()
-
-        while now == 10
-                || now == 32 { now = read() }
-
-        while now != 10
-                && now != 32 && now != 0 {
-            str += String(bytes: [now], encoding: .ascii)!
-            now = read()
-        }
-
-        return str
-    }
+    return answer
 }
 
-var file = FileIO()
-var N = file.readInt()
-var able = Array(repeating: 0, count: N)
-var crane = Array(repeating: 0, count: N)
-for i in 0..<N {
-    crane[i] = file.readInt()
-}
-var M = file.readInt()
-var box = Array(repeating: 0, count: M)
-for i in 0..<M {
-    box[i] = file.readInt()
-}
-
-var answer = 0
-var index = 0
-
-crane.sort(by:>)
-box.sort(by:>)
-
-if box[0] > crane[0] {
-    answer = -1
-}
-for i in 0..<num {
-    if crane[index] >= box[i] {
-        able[index] += 1
-    } else {
-        index += 1
-        i -= 1
-    }
-}
-while true {
-    for i in 
-}
-
-print(answer)
+print(solve())
